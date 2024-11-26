5766c06812c9f1753147c0f8c47fa80114e520cf
Abhishek Maurya <mauryaabhishek1508@gmail.com>
2024-11-26 16:48:01 +0530

    fixed overflow issue

--- src/App.tsx
+++ src/App.tsx
@@ -50,9 +50,10 @@ const App = () => {
     <>
     <div className={darkmode?'dark':''}>
       <div className="h-screen w-full fixed backdrop-blur-md pointer-events-none z-5 bg-red-200/20"></div>
-      <div className={`transition-all duration-300 ease-in-out ${darkmode ? 'dark:bg-gray-900' : 'bg-mainbg/60 text-black'} w-full h-full flex items-center justify-center relative z-1`}>
+      <div className={`transition-all duration-300 ease-in-out ${darkmode ? 
+        'dark:bg-gray-900' : 'bg-mainbg/60 text-black'} w-full h-full flex items-center justify-center relative z-1 overflow-x-hidden`}>
 
-      <main className="h-full w-full mx-5 md:mx-20 lg:mx-32 2xl:mx-75 relative z-10">
+      <main className="h-full w-full mx-5 md:mx-20 lg:mx-32 2xl:mx-75 relative z-10 overflow-x-hidden">
 
         <Nav darkmode={darkmode}/>
         <Intro darkmode={darkmode} />
@@ -64,7 +65,7 @@ const App = () => {
         <Footer />
         <div className=' flex items-center justify-end w-full bg-red-200 h-50'>
 
-        <div className='fixed bottom-5 left-72 right:0 md:left-96 flex items-center justify-center w-10 h-10 bg-white/60 rounded-full z-50'>
+        <div className='fixed bottom-5 right-[1rem] md:right-[3rem]  flex items-center justify-center w-10 h-10 bg-white/60 rounded-full z-50'>
            <BsFillMoonStarsFill onClick={handledarkmode}/>
         </div>
         </div>
